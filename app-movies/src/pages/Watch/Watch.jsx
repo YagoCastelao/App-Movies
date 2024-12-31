59e9b4c4fe06a42ccaca6fa110d50184b1cb3f95
Yago Cima Castelao <yago_castelau@hotmail.com>
2024-12-31 02:34:12 +0100

    Applicacion Separada en paginas y creado roteamento

--- app-movies/src/pages/Watch/Watch.jsx
+++ app-movies/src/pages/Watch/Watch.jsx
@@ -1,22 +1,30 @@
-import styles from './Watch.module.css'
+import styles from "./Watch.module.css";
 
-import Header from '../../components/Header/Header'
-import Container from '../../components/Container/Container'
-import Footer from '../../components/Footer/Footer'
+import Header from "../../components/Header/Header";
+import Container from "../../components/Container/Container";
+import Footer from "../../components/Footer/Footer";
 
 function Watch() {
-
-    return (
-        <>
-            <Header />
-            <Container>
-                <section className={styles.watch}>
-                    <h1>Assistir</h1>
-                </section>
-            </Container>
-            <Footer />
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <Container>
+        <section className={styles.watch}>
+          <iframe
+            width="854"
+            height="480"
+            src="https://www.youtube.com/embed/ga9irA-sBKY?si=1F3AmG2Fgmde_1SL"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        </section>
+      </Container>
+      <Footer />
+    </>
+  );
 }
 
-export default Watch
+export default Watch;
