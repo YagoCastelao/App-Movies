4036ac6dfc1c27721248e346410038ec96705de2
Yago Cima Castelao <yago_castelau@hotmail.com>
2025-01-04 00:09:51 +0100

    Componente de Carousel creado con React-Slick

---
+++ app-movies/src/components/Carousel/Carousel.jsx
@@ -0,0 +1,24 @@
+import "./carousel.css";
+
+import Slider from "react-slick";
+
+function Carousel({ children }) {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    centerMode: false,
+    variableWidth: true,
+    adaptiveHeight: true,
+    // slidesToShow: 5,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div>
+      <Slider {...settings}>{children}</Slider>
+    </div>
+  );
+}
+
+export default Carousel;
--- app-movies/src/pages/Home/Home.jsx
+++ app-movies/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Card from "../../components/Card/Card";
 import Container from "../../components/Container/Container";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import Carousel from '../../components/Carousel/Carousel'
 import Category, {
   categories,
   filterCategory,
@@ -16,9 +17,11 @@ function Home() {
       <Container>
         {categories.map((category, index) => (
           <Category key={index} category={category}>
+            <Carousel>
             {filterCategory(index).map((video) => (
               <Card id={video.id} key={video.id} />
             ))}
+            </Carousel> 
           </Category>
         ))}
       </Container>
